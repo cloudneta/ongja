5b0e333b4bca7af7928375f3a65cc88514aa6fca
cloudneta <66163453+cloudneta@users.noreply.github.com>
2026-07-18 22:57:51 +0900

    Create create-sg-lab-resources.sh
    created

---
+++ create-sg-lab-resources.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+VPC_ID=$(aws ec2 describe-vpcs \
+  --filters "Name=tag:Name,Values=cnasg-VPC" \
+  --query 'Vpcs[0].VpcId' \
+  --output text)
+
+# 정상 보안그룹 생성 (COMPLIANT 대상)
+aws ec2 create-security-group \
+  --group-name cnasg-normal-sg \
+  --description "Normal SG for Config test" \
+  --vpc-id ${VPC_ID} \
+  --tag-specifications "ResourceType=security-group,Tags=[{Key=Lab,Value=cnasg-16}]"
+
+NORMAL_SG_ID=$(aws ec2 describe-security-groups \
+  --filters "Name=group-name,Values=cnasg-normal-sg" \
+  --query 'SecurityGroups[0].GroupId' \
+  --output text)
+
+aws ec2 authorize-security-group-ingress \
+  --group-id ${NORMAL_SG_ID} \
+  --protocol tcp --port 22 --cidr $(curl -s ipinfo.io/ip)/32
+
+# 취약 보안그룹 생성 (NON_COMPLIANT 대상)
+aws ec2 create-security-group \
+  --group-name cnasg-vulnerable-sg \
+  --description "Vulnerable SG for Config test" \
+  --vpc-id ${VPC_ID} \
+  --tag-specifications "ResourceType=security-group,Tags=[{Key=Lab,Value=cnasg-16}]"
+
+VULN_SG_ID=$(aws ec2 describe-security-groups \
+  --filters "Name=group-name,Values=cnasg-vulnerable-sg" \
+  --query 'SecurityGroups[0].GroupId' \
+  --output text)
+
+aws ec2 authorize-security-group-ingress \
+  --group-id ${VULN_SG_ID} \
+  --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+echo "NORMAL_SG_ID: ${NORMAL_SG_ID}"
+echo "VULN_SG_ID: ${VULN_SG_ID}"
+echo "Done."
